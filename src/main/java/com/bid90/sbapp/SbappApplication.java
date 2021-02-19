cbc47241eaa4a415e38d29467e347738d5803877
Daniel Bradea <daniel.bradea@ferendia.com>
2021-02-19 18:40:49 +0200

    init . Closes #1.

---
+++ src/main/java/com/bid90/sbapp/SbappApplication.java
@@ -0,0 +1,13 @@
+package com.bid90.sbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbappApplication.class, args);
+    }
+
+}
